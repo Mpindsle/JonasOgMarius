1521b8da5c0dbda9197de0f18d9ea9a15041332f
Jonas-bro <41417575+Jonas-bro@users.noreply.github.com>
2018-07-19 09:03:53 +0200

    Update Adventure.java

--- Adventure.java
+++ Adventure.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 public class Bro {
-		public static void main(String[] args){
+	public static void main(String[] args){
 			
-			Scanner sc1 = new Scanner(System.in);
+		Scanner sc1 = new Scanner(System.in);
 			
-			System.out.println("Hei! Hva heter du?");
+		System.out.println("Hei! Hva heter du?");
 			
-			String svar = sc1.nextLine();
+		String svar = sc1.nextLine();
 			
-			if(svar = "Marius") {
-        System.out.println("Marius is in. We're locked and loaded.");
-      }else{
-        System.out.println("Hmm ok.. Kanskje du kan finne en som heter Marius?");
-      }
-		}
+		if(svar = "Marius") {
+       			System.out.println("Marius is in. We're locked and loaded.");
+		}else{
+       			System.out.println("Hmm ok.. Kanskje du kan finne en som heter Marius?");
+      		}
 	}
+}
